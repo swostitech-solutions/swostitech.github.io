4b71e5cc56ec14b83f620746ccc9963b584763c1
swostitech-solutions <118847264+swostitech-solutions@users.noreply.github.com>
2022-11-26 00:18:53 +0530

    commit

---
+++ js/resp.js
@@ -0,0 +1,9 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+navList=document.querySelector('.nav-list')
+
+
+burger.addEventListener('click',()=>{
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
