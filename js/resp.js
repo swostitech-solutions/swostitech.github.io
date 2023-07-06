8b1a5e1d30661a397e70eee5770a3eb4c3866623
mjena <mjena@mmitnetwork.com>
2023-07-06 18:30:32 +0530

    commit

--- js/resp.js
+++
@@ -1,8 +0,0 @@
-burger=document.querySelector('.burger')
-navbar=document.querySelector('.navbar')
-navList=document.querySelector('.nav-list')
-
-burger.addEventListener('click',()=>{
-    navList.classList.toggle('v-class-resp');
-    navbar.classList.toggle('h-nav-resp');
-})
